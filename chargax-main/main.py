f47598946a9640c0dc37f9e63447189452463788
likhit2002 <53993767+likhit2002@users.noreply.github.com>
2025-10-03 13:44:57 +0200

    Update main.py

--- chargax-main/main.py
+++ chargax-main/main.py
@@ -2,6 +2,7 @@ import jax
 import jaxnasium as jym
 import numpy as np
 import optax
+#test
 from jaxnasium.algorithms import PPO
 
 from chargax import Chargax, get_electricity_prices  # noqa: E402
